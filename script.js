35f155783eb06a803dd001e682f2f15fbb43e3db
jamie rolls <jamie120@hotmail.com>
2021-06-01 12:51:26 +0100

    Added HTML content, script and css

---
+++ script.js
@@ -0,0 +1,34 @@
+var button = document.querySelector('button');
+var input = document.querySelector('input');
+var list = document.querySelector('.list-items')
+var listItems = []
+
+
+button.addEventListener('click', function() {
+    if (input.value !== "") {
+        var li = document.createElement('li');
+        li.innerText = input.value;
+        list.append(li);
+        input.value = "";
+        var dBtn = document.createElement('button')
+        dBtn.appendChild(document.createTextNode('X'));
+        dBtn.classList.add("delete-btn");
+        li.appendChild(dBtn);
+        dBtn.addEventListener('click', deleteListItem);
+
+        function deleteListItem() {
+            li.classList.add('delete')
+        }
+
+        li.addEventListener('click', function() {
+            this.classList.toggle("complete");
+            dBtn.classList.toggle('complete')
+        })
+
+    } else {
+        alert("You have not typed anything into the input box!")
+    }
+})
+
+
+
